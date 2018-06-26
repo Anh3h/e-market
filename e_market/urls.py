a3b4b47ef7afe2f68b216b3c925ae800e4cdb4cd
Anh3h <courageangeh@gmail.com>
2018-06-26 15:21:06 +0100

    Fix bu with redirect to login from logout button and hide login link after a successful login

--- e_market/urls.py
+++ e_market/urls.py
@@ -22,7 +22,7 @@ from purchase import views as core_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', core_views.signup, name='signup'),
-    path('login/', auth_views.login, {'template_name': 'core/login.html'}, name='login'),
+    path('login/', auth_views.login, {'template_name': 'core/auth/login.html'}, name='login'),
     path('logout/', auth_views.logout, {'next_page': 'home'}, name='logout'),
     path('', core_views.home, name="home"),
     path('profile/', include('custom_profile.urls')),
