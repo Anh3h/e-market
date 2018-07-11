7f854cd2fc122ca49038da8f315f2fb982cea7a5
Anh3h <courageangeh@gmail.com>
2018-07-11 08:54:56 +0100

    Add create product page

--- product/urls.py
+++ product/urls.py
@@ -6,5 +6,6 @@ app_name = "product"
 urlpatterns = [
     path('', views.view_my_products, name='viewMyProducts'),
     path('<int:product_id>/', views.view_product, name='viewProduct'),
-    path('edit/<int:product_id>', views.edit_product, name='editProduct')
+    path('edit/<int:product_id>', views.edit_product, name='editProduct'),
+    path('create/', views.create_product, name='createProduct')
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from custom_profile.models import Profile
 from product.models import Measurement, Category, Logistic
 from purchase.models import Product
 
@@ -27,12 +28,37 @@ def edit_product(request, product_id):
         product.price = request.POST.get("price", "")
         product.quantity = request.POST.get("quantity", "")
         product.measurement_id = Measurement.objects.get(pk=request.POST.get("measurement_id", ""))
-        product.category_id = Category.objects.get(pk=request.POST.get("logistic_id", ""))
+        product.category_id = Category.objects.get(pk=request.POST.get("category_id", ""))
         product.logistic_id = Logistic.objects.get(pk=request.POST.get("logistic_id", ""))
         product.description = request.POST.get("description", "")
         product.save()
         return redirect('product:viewProduct', product_id)
     return render(request, 'core/partial/edit_product.html',
+                  {'product': product,
+                   'measurements': measurements,
+                   'logistics': logistics,
+                   'categories': categories})
+
+
+@login_required
+def create_product(request):
+    product = Product()
+    measurements = Measurement.objects.all()
+    categories = Category.objects.all()
+    logistics = Logistic.objects.all()
+    if request.method == 'POST':
+        product.name = request.POST.get("name", "")
+        product.price = request.POST.get("price", "")
+        product.quantity = request.POST.get("quantity", "")
+        product.image_link = "link"
+        product.user_id = Profile.objects.get(pk=request.user.id)
+        product.measurement_id = Measurement.objects.get(pk=request.POST.get("measurement_id", ""))
+        product.category_id = Category.objects.get(pk=request.POST.get("logistic_id", ""))
+        product.logistic_id = Logistic.objects.get(pk=request.POST.get("logistic_id", ""))
+        product.description = request.POST.get("description", "")
+        product.save()
+        return redirect('product:viewMyProducts')
+    return render(request, 'core/partial/create_product.html',
                   {'product': product,
                    'measurements': measurements,
                    'logistics': logistics,
